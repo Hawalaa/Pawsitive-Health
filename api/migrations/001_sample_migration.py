ac3dd8aa0a3d58a35bc3fcbb7179abec334cfc67
Junhao Liang <leongjunho@gmail.com>
2023-09-27 01:47:00 +0000

    Resolve "Create poop/sleeping db table"

--- api/migrations/001_sample_migration.py
+++ api/migrations/001_sample_migration.py
@@ -21,13 +21,13 @@ steps = [
         # "Up" SQL statement
         """
         CREATE TABLE pet (
-            id SERIAL PRIMARY KEY NOT NULL,
-            pet_id UUID NOT NULL,
+            id SERIAL NOT NULL,
+            pet_id UUID PRIMARY KEY NOT NULL,
             pet_name VARCHAR(100) NOT NULL,
             breed VARCHAR(100) NOT NULL,
             gender VARCHAR(100) NOT NULL,
             age INTEGER NOT NULL,
-            weight INTEGER NOT NULL, 
+            weight INTEGER NOT NULL,
             user_id UUID NOT NULL,
             FOREIGN KEY (user_id) REFERENCES users (user_id)
         );
@@ -35,6 +35,42 @@ steps = [
         # "Down" SQL statement
         """
         DROP TABLE pet;
+        """,
+    ],
+    [
+        # "Up" SQL statement
+        """
+        CREATE TABLE poop (
+            id SERIAL PRIMARY KEY NOT NULL,
+            poop_id UUID NOT NULL,
+            date DATE NOT NULL,
+            time TIME NOT NULL,
+            consistency VARCHAR(100) NOT NULL,
+            pet_id UUID NOT NULL,
+            FOREIGN KEY (pet_id) REFERENCES pet (pet_id)
+        );
+        """,
+        # "Down" SQL statement
+        """
+        DROP TABLE poop;
+        """,
+    ],
+    [
+        # "Up" SQL statement
+        """
+        CREATE TABLE sleep (
+            id SERIAL PRIMARY KEY NOT NULL,
+            sleep_id UUID NOT NULL,
+            date DATE NOT NULL,
+            time TIME NOT NULL,
+            duration VARCHAR(100) NOT NULL,
+            pet_id UUID NOT NULL,
+            FOREIGN KEY (pet_id) REFERENCES pet (pet_id)
+        );
+        """,
+        # "Down" SQL statement
+        """
+        DROP TABLE sleep;
         """,
     ],
 ]
