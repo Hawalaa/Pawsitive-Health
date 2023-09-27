d2820743f9bfd9331b1414cc3e7e3104a06f013b
Ken Yeh <yehken@gmail.com>
2023-09-27 02:03:18 +0000

    Resolve "Create walk/feeding db table"

--- api/migrations/001_sample_migration.py
+++ api/migrations/001_sample_migration.py
@@ -74,3 +74,115 @@ steps = [
         """,
     ],
 ]
+steps = [
+    [
+        # "Up" SQL statement
+        """
+        CREATE TABLE users (
+            id SERIAL NOT NULL,
+            user_id UUID PRIMARY KEY NOT NULL,
+            username VARCHAR(100) NOT NULL,
+            first_name VARCHAR(100) NOT NULL,
+            last_name VARCHAR(100) NOT NULL,
+            email VARCHAR(100) NOT NULL,
+            password VARCHAR(100) NOT NULL
+        );
+        """,
+        # "Down" SQL statement
+        """
+        DROP TABLE users;
+        """,
+    ],
+    [
+        # "Up" SQL statement
+        """
+        CREATE TABLE pet (
+            id SERIAL NOT NULL,
+            pet_id UUID PRIMARY KEY NOT NULL,
+            pet_name VARCHAR(100) NOT NULL,
+            breed VARCHAR(100) NOT NULL,
+            gender VARCHAR(100) NOT NULL,
+            age INTEGER NOT NULL,
+            weight INTEGER NOT NULL,
+            user_id UUID NOT NULL,
+            FOREIGN KEY (user_id) REFERENCES users (user_id)
+        );
+        """,
+        # "Down" SQL statement
+        """
+        DROP TABLE pet;
+        """,
+    ],
+    [
+        # "Up" SQL statement
+        """
+        CREATE TABLE poop (
+            id SERIAL PRIMARY KEY NOT NULL,
+            poop_id UUID NOT NULL,
+            date DATE NOT NULL,
+            time TIME NOT NULL,
+            consistency VARCHAR(100) NOT NULL,
+            pet_id UUID NOT NULL,
+            FOREIGN KEY (pet_id) REFERENCES pet (pet_id)
+        );
+        """,
+        # "Down" SQL statement
+        """
+        DROP TABLE poop;
+        """,
+    ],
+    [
+        # "Up" SQL statement
+        """
+        CREATE TABLE sleep (
+            id SERIAL PRIMARY KEY NOT NULL,
+            sleep_id UUID NOT NULL,
+            date DATE NOT NULL,
+            time TIME NOT NULL,
+            duration VARCHAR(100) NOT NULL,
+            pet_id UUID NOT NULL,
+            FOREIGN KEY (pet_id) REFERENCES pet (pet_id)
+        );
+        """,
+        # "Down" SQL statement
+        """
+        DROP TABLE sleep;
+        """,
+    ],
+    [
+        # "Up" SQL statement
+        """
+        CREATE TABLE walk (
+            id SERIAL NOT NULL,
+            walk_id UUID PRIMARY KEY NOT NULL,
+            date DATE NOT NULL,
+            time TIME NOT NULL,
+            duration VARCHAR(100) NOT NULL,
+            pet_id UUID NOT NULL,
+            FOREIGN KEY (pet_id) REFERENCES pet (pet_id)
+        );
+        """,
+        # "Down" SQL statement
+        """
+        DROP TABLE walk;
+        """,
+    ],
+    [
+        # "Up" SQL statement
+        """
+        CREATE TABLE feeding (
+            id SERIAL NOT NULL,
+            feeding_id UUID PRIMARY KEY NOT NULL,
+            date DATE NOT NULL,
+            time TIME NOT NULL,
+            duration VARCHAR(100) NOT NULL,
+            pet_id UUID NOT NULL,
+            FOREIGN KEY (pet_id) REFERENCES pet (pet_id)
+        );
+        """,
+        # "Down" SQL statement
+        """
+        DROP TABLE feeding;
+        """,
+    ],
+]
