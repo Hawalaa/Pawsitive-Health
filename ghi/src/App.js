182df12012a82683db03b4806220cf3069163569
Ken Yeh <yehken@gmail.com>
2023-10-12 18:32:00 -0700

    saving current status

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,37 +1,54 @@
-import { useEffect, useState } from "react";
-import Construct from "./Construct.js";
-import ErrorNotification from "./ErrorNotification";
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ListPet from "./Components/PetProfile"
+// import Construct from "./Construct.js";
+// import ErrorNotification from "./ErrorNotification";
+// import "./App.css";
+
 
 function App() {
-  const [launchInfo, setLaunchInfo] = useState([]);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    async function getData() {
-      let url = `${process.env.REACT_APP_API_HOST}/api/launch-details`;
-      console.log("fastapi url: ", url);
-      let response = await fetch(url);
-      console.log("------- hello? -------");
-      let data = await response.json();
-
-      if (response.ok) {
-        console.log("got launch data!");
-        setLaunchInfo(data.launch_details);
-      } else {
-        console.log("drat! something happened");
-        setError(data.message);
-      }
-    }
-    getData();
-  }, []);
-
   return (
-    <div>
-      <ErrorNotification error={error} />
-      <Construct info={launchInfo} />
-    </div>
+    <BrowserRouter>
+      <div className="container">
+        <Routes>
+          <Route path="/pet" element={<ListPet />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+
+// function App() {
+//   const [launchInfo, setLaunchInfo] = useState([]);
+//   const [error, setError] = useState(null);
+
+//   useEffect(() => {
+//     async function getData() {
+//       let url = `${process.env.REACT_APP_API_HOST}/api/launch-details`;
+//       console.log("fastapi url: ", url);
+//       let response = await fetch(url);
+//       console.log("------- hello? -------");
+//       let data = await response.json();
+
+//       if (response.ok) {
+//         console.log("got launch data!");
+//         setLaunchInfo(data.launch_details);
+//       } else {
+//         console.log("drat! something happened");
+//         setError(data.message);
+//       }
+//     }
+//     getData();
+//   }, []);
+
+//   return (
+//     <div>
+//       <ErrorNotification error={error} />
+//       <Construct info={launchInfo} />
+//     </div>
+//   );
+// }
+
+// export default App;
