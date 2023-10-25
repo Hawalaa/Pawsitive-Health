848714932bb0a09826cb1e4c7ec76f73105c4312
Cory DeGuzman <deguzmancory@ymail.com>
2023-10-25 16:49:10 +0000

    Resolve "Navigation (Refactor): Fix base url redirect"

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -22,7 +22,7 @@ export default function App() {
 				<AuthProvider>
 					<BrowserRouter basename={basename}>
 						<Routes>
-							<Route path="/" element={<Dashboard />} />
+							<Route path="/" element={<LoginForm />} />
 							<Route path="/dashboard" element={<Dashboard />} />
 							<Route path="/login" element={<LoginForm />} />
 							<Route path="/signup" element={<SignupForm />} />
